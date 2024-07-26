cc6063a4be53b0cb89d636fb8c71e29efe1b9f52
fahmi <amixcustomlinux@gmail.com>
2024-07-26 22:37:40 +0700

    [DEV] DB Migration: Create User Table

--- database/migrations/2024_07_26_152551_create_users_table.php
+++ database/migrations/2024_07_26_152551_create_users_table.php
@@ -12,13 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->integerIncrements('id');
+            $table->string('full_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->unsignedTinyInteger('type')->comment('1 = Admin; 2 = User;');
             $table->timestamps();
+            $table->unsignedInteger('created_by')->nullable();
+            $table->unsignedInteger('updated_by')->nullable();
         });
     }
 
