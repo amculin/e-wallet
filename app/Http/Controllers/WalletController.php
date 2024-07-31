3ede387fc06ddf2434a3aa64986e16df07dbeb8b
fahmi <amixcustomlinux@gmail.com>
2024-07-31 11:52:16 +0700

    [DEV] Wallet: Auth checker revision, Simulate calling payment API

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -7,6 +7,7 @@ use App\Models\Transaction;
 use App\Models\UserAccount;
 use App\Models\Users;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class WalletController extends Controller
 {
@@ -14,16 +15,25 @@ class WalletController extends Controller
     {
         $data = Users::findUserByToken($token);
 
-        if ($data) {
-            return $data;
-        } else {
-            return response(['message' => 'Invalid credential!'], 401);
-        }
+        return $data;
+    }
+
+    public function makePaymentRequest($token, $data)
+    {
+        $url = env('PAYMENT_URL');
+        $response = Http::withToken($token)->get($url, $data);
+
+        return $response;
     }
 
     public function index(Request $request)
     {
         $user = $this->authChecker($request->bearerToken());
+
+        if (!$user) {
+            return response(['message' => 'Invalid credential!'], 401);
+        }
+
         $data = Wallet::getBalance($user->id);
         $account = UserAccount::getAccount($user->id);
 
@@ -40,111 +50,156 @@ class WalletController extends Controller
     public function deposit(Request $request)
     {
         $user = $this->authChecker($request->bearerToken());
+
+        if (!$user) {
+            return response(['message' => 'Invalid credential!'], 401);
+        }
+
         $wallet = Wallet::getBalance($user->id);
 
-        $additionalData = $request->input('additional_data');
-        $additionalData['client_time'] = $request->input('timestamp');
+        $paymentResponse = $this->makePaymentRequest($request->bearerToken(), [
+            'order_id' => $request->input('order_id'),
+            'amount' => $request->input('amount'),
+            'timestamp' => $request->input('timestamp')
+        ]);
 
-        $transaction = new Transaction;
-        $transaction->wallet_id = $wallet->id;
-        $transaction->order_id = $request->input('order_id');
-        $transaction->amount = $request->input('amount');
-        $transaction->type = Transaction::DEPOSIT;
-        $transaction->status = Transaction::IS_SUCCESS;
-        $transaction->additional_data = json_encode($additionalData);
-        $transaction->created_by = $user->id;
-        $transaction->save();
-
-        $wallet->balance = $wallet->balance + $transaction->amount;
-        $wallet->updated_by = $user->id;
-        $wallet->save();
-
-        return [
-            'order_id' => $transaction->order_id,
-            'amount' => $transaction->amount,
-            'status' => $transaction->status
-        ];
+        if ($paymentResponse->ok()) {
+            $additionalData = $request->input('additional_data');
+            $additionalData['client_time'] = $request->input('timestamp');
+    
+            $transaction = new Transaction;
+            $transaction->wallet_id = $wallet->id;
+            $transaction->order_id = $request->input('order_id');
+            $transaction->amount = $request->input('amount');
+            $transaction->type = Transaction::DEPOSIT;
+            $transaction->status = Transaction::IS_SUCCESS;
+            $transaction->additional_data = json_encode($additionalData);
+            $transaction->created_by = $user->id;
+            $transaction->save();
+    
+            $wallet->balance = $wallet->balance + $transaction->amount;
+            $wallet->updated_by = $user->id;
+            $wallet->save();
+    
+            return [
+                'order_id' => $transaction->order_id,
+                'amount' => $transaction->amount,
+                'status' => $transaction->status
+            ];
+        } else {
+            return response(['message' => 'Error while connecting with payment gateway!'], 500);
+        }
     }
 
     public function withdrawal(Request $request)
     {
         $user = $this->authChecker($request->bearerToken());
+
+        if (!$user) {
+            return response(['message' => 'Invalid credential!'], 401);
+        }
+
         $wallet = Wallet::getBalance($user->id);
 
-        $additionalData = $request->input('additional_data');
-        $additionalData['client_time'] = $request->input('timestamp');
+        $paymentResponse = $this->makePaymentRequest($request->bearerToken(), [
+            'order_id' => $request->input('order_id'),
+            'amount' => $request->input('amount'),
+            'timestamp' => $request->input('timestamp')
+        ]);
 
-        if ($request->amount > $wallet->balance) {
-            $status = Transaction::IS_FAILED;
-            $updateBalance = false;
+        if ($paymentResponse->ok()) {
+            $additionalData = $request->input('additional_data');
+            $additionalData['client_time'] = $request->input('timestamp');
+
+            if ($request->amount > $wallet->balance) {
+                $status = Transaction::IS_FAILED;
+                $updateBalance = false;
+            } else {
+                $status = Transaction::IS_SUCCESS;
+                $updateBalance = true;
+            }
+
+            $transaction = new Transaction;
+            $transaction->wallet_id = $wallet->id;
+            $transaction->order_id = $request->input('order_id');
+            $transaction->amount = $request->input('amount');
+            $transaction->type = Transaction::WITHDRAWAL;
+            $transaction->status = $status;
+            $transaction->additional_data = json_encode($additionalData);
+            $transaction->created_by = $user->id;
+            $transaction->save();
+
+            if ($updateBalance) {
+                $wallet->balance = $wallet->balance - $transaction->amount;
+                $wallet->updated_by = $user->id;
+                $wallet->save();
+            } else {
+                return response(['message' => 'Insufficient Balance!'], 422);
+            }
+
+            return [
+                'order_id' => $transaction->order_id,
+                'amount' => $transaction->amount,
+                'status' => $status
+            ];
         } else {
-            $status = Transaction::IS_SUCCESS;
-            $updateBalance = true;
+            return response(['message' => 'Error while connecting with payment gateway!'], 500);
         }
-
-        $transaction = new Transaction;
-        $transaction->wallet_id = $wallet->id;
-        $transaction->order_id = $request->input('order_id');
-        $transaction->amount = $request->input('amount');
-        $transaction->type = Transaction::WITHDRAWAL;
-        $transaction->status = $status;
-        $transaction->additional_data = json_encode($additionalData);
-        $transaction->created_by = $user->id;
-        $transaction->save();
-
-        if ($updateBalance) {
-            $wallet->balance = $wallet->balance - $transaction->amount;
-            $wallet->updated_by = $user->id;
-            $wallet->save();
-        } else {
-            return response(['message' => 'Insufficient Balance!'], 422);
-        }
-
-        return [
-            'order_id' => $transaction->order_id,
-            'amount' => $transaction->amount,
-            'status' => $status
-        ];
     }
 
     public function purchase(Request $request)
     {
         $user = $this->authChecker($request->bearerToken());
+
+        if (!$user) {
+            return response(['message' => 'Invalid credential!'], 401);
+        }
+
         $wallet = Wallet::getBalance($user->id);
 
-        $additionalData = $request->input('additional_data');
-        $additionalData['client_time'] = $request->input('timestamp');
+        $paymentResponse = $this->makePaymentRequest($request->bearerToken(), [
+            'order_id' => $request->input('order_id'),
+            'amount' => $request->input('amount'),
+            'timestamp' => $request->input('timestamp')
+        ]);
 
-        if ($request->amount > $wallet->balance) {
-            $status = Transaction::IS_FAILED;
-            $updateBalance = false;
+        if ($paymentResponse->ok()) {
+            $additionalData = $request->input('additional_data');
+            $additionalData['client_time'] = $request->input('timestamp');
+
+            if ($request->amount > $wallet->balance) {
+                $status = Transaction::IS_FAILED;
+                $updateBalance = false;
+            } else {
+                $status = Transaction::IS_SUCCESS;
+                $updateBalance = true;
+            }
+
+            $transaction = new Transaction;
+            $transaction->wallet_id = $wallet->id;
+            $transaction->order_id = $request->input('order_id');
+            $transaction->amount = $request->input('amount');
+            $transaction->type = Transaction::PURCHASE;
+            $transaction->status = $status;
+            $transaction->additional_data = json_encode($additionalData);
+            $transaction->created_by = $user->id;
+            $transaction->save();
+
+            if ($updateBalance) {
+                $wallet->balance = $wallet->balance - $transaction->amount;
+                $wallet->updated_by = $user->id;
+                $wallet->save();
+            } else {
+                return response(['message' => 'Insufficient Balance!'], 422);
+            }
+
+            return [
+                'order_id' => $transaction->order_id,
+                'amount' => $transaction->amount,
+                'status' => $status
+            ];
         } else {
-            $status = Transaction::IS_SUCCESS;
-            $updateBalance = true;
+            return response(['message' => 'Error while connecting with payment gateway!'], 500);
         }
-
-        $transaction = new Transaction;
-        $transaction->wallet_id = $wallet->id;
-        $transaction->order_id = $request->input('order_id');
-        $transaction->amount = $request->input('amount');
-        $transaction->type = Transaction::PURCHASE;
-        $transaction->status = $status;
-        $transaction->additional_data = json_encode($additionalData);
-        $transaction->created_by = $user->id;
-        $transaction->save();
-
-        if ($updateBalance) {
-            $wallet->balance = $wallet->balance - $transaction->amount;
-            $wallet->updated_by = $user->id;
-            $wallet->save();
-        } else {
-            return response(['message' => 'Insufficient Balance!'], 422);
-        }
-
-        return [
-            'order_id' => $transaction->order_id,
-            'amount' => $transaction->amount,
-            'status' => $status
-        ];
     }
 }
